bac66ae3d629bff5cfb8f7847878d201fc0123db
DannyEst6109 <72481675+DannyEst6109@users.noreply.github.com>
2024-02-11 17:37:43 -0600

    act

--- import.py
+++ import.py
@@ -1,13 +1,20 @@
 import pandas as pd
 from pymongo import MongoClient
 
+# Reemplaza 'usuario' y 'contraseña' con tus credenciales de MongoDB Atlas
+usuario = "est20853"
+contraseña = "<BqzE2nF1pib6pc8z>"
+cadena_conexion = f"mongodb+srv://{usuario}:{contraseña}@proyect1.eolzon9.mongodb.net/"
+
 # Conectar a MongoDB Atlas
-client = MongoClient("tmongodb+srv://est20853:<BqzE2nF1pib6pc8z>@proyect1.eolzon9.mongodb.net/")
+client = MongoClient(cadena_conexion)
+
+# Conectar a MongoDB Atlas
 db = client["Lab03"]
 collection = db["movies"]
 
 # Cargar datos desde el archivo .csv
-data = pd.read_csv("movies.csv")
+data = pd.read_csv("CCGeneral1.csv")
 
 # Convertir el DataFrame a formato JSON
 data_json = data.to_dict(orient='records')
