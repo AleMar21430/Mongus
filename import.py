fde4a1c8b04c497876b5387d8a65f91310c585bb
Josúe_Samuel_A.H <87200735+Gustixa@users.noreply.github.com>
2024-02-11 18:21:14 -0600

    Update import.py

--- import.py
+++ import.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 # Reemplaza 'usuario' y 'contraseña' con tus credenciales de MongoDB Atlas
 usuario = "est20853"
 contraseña = "<BqzE2nF1pib6pc8z>"
-cadena_conexion = f"mongodb+srv://{usuario}:{contraseña}@proyect1.eolzon9.mongodb.net/"
+cadena_conexion = f"mongodb+srv://admin2:211024@proyect1.eolzon9.mongodb.net/"
 
 # Conectar a MongoDB Atlas
 client = MongoClient(cadena_conexion)
